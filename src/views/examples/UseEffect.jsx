f9b1e126468d67a9710566cf808b1c1554943763
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-05-30 08:25:36 -0300

    useEffect #02

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,29 +1,42 @@
 import React, { useEffect, useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 function calcFatorial(num) {
-    if (!num) return 1
-    const n = parseInt(num)
-    if (n < 0) return -1
-    if(n === 0) return 1
-    return calcFatorial(n - 1) * n
+  if (!num) return 1;
+  const n = parseInt(num);
+  if (n < 0) return -1;
+  if (n === 0) return 1;
+  return calcFatorial(n - 1) * n;
 }
 
 const UseEffect = (props) => {
+  //Ex #01
   const [number, setNumber] = useState(1);
   const [fatorial, setFatorial] = useState(1);
 
-  useEffect(function() {
-      setFatorial(calcFatorial(number))
+  useEffect(
+    function () {
+      setFatorial(calcFatorial(number));
+    },
+    [number]
+  );
+
+  useEffect(
+    function () {
+      if (fatorial > 100000) {
+        document.title = "Eita!!!";
+      }
+    },
+    [fatorial]
+  );
+  //Ex #01
+
+  const [status, setStatus] = useState("Ímpar");
+
+  useEffect(function(){
+      setStatus(number % 2 === 0 ? "Par" : "Ímpar")
   }, [number])
-
-  useEffect(function() {
-
-    if(fatorial > 100000) {
-        document.title = 'Eita!!!'
-    }
-
-  }, [fatorial])
 
   return (
     <div className="UseEffect">
@@ -32,10 +45,14 @@ const UseEffect = (props) => {
         subtitle="Permite executar efeitos colaterais em componentes funcionais!"
       />
 
+      <SectionTitle title="Exercício #01" />
+
       <div className="center">
         <div>
           <span className="text">Fatorial: </span>
-          <span className="text red">{fatorial === -1 ? 'Não Existe :( ' : fatorial}</span>
+          <span className="text red">
+            {fatorial === -1 ? "Não Existe :( " : fatorial}
+          </span>
         </div>
         <input
           type="number"
@@ -43,6 +60,13 @@ const UseEffect = (props) => {
           value={number}
           onChange={(e) => setNumber(e.target.value)}
         />
+      </div>
+      <SectionTitle title="Exercício #02" />
+      <div className="center">
+        <div>
+          <span className="text">Status: </span>
+          <span className="text red">{status}</span>
+        </div>
       </div>
     </div>
   );
